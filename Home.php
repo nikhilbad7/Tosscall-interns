db246babcd253ea7dd5e7a516e94fa4864acdc04
nikhil <nikhil.badani@gmail.com>
2018-06-14 19:00:27 +0530

    first commit

---
+++ Home.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+			<title> HOME </title>
+	</head>
+	<body>
+		<h1 align="center">DISCUSSION FORUM</h1>
+		<ul>
+			<li><a href="#">Start</a></li>
+			<li><a href="#">Select</a></li>
+			<li><a href="#">Watch</a></li>
+			<li><a href="#">List</a></li>
+			<li><a href="#">Notification</a></li>
+			<li><a href="#">MyDiscussion</a></li>
+		</ul>
+	</body>
+</html>
