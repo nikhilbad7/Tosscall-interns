41ece524ace2d6799a588a65f26228afa4f483b0
nikhil <nikhil.badani@gmail.com>
2018-06-15 13:52:04 +0530

    sql file added

---
+++ tosscall.sql
@@ -0,0 +1,308 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 15, 2018 at 10:10 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tosscall`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `City`
+--
+
+CREATE TABLE `City` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `state_id` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Event`
+--
+
+CREATE TABLE `Event` (
+  `id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `eventtype_id` int(11) NOT NULL,
+  `favour` varchar(30) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `city_id` int(11) NOT NULL,
+  `init_user_id` int(11) NOT NULL,
+  `acce_user_id` int(11) DEFAULT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `EventType`
+--
+
+CREATE TABLE `EventType` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Likes`
+--
+
+CREATE TABLE `Likes` (
+  `id` int(11) NOT NULL,
+  `message_id` int(11) NOT NULL,
+  `liker_user_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Message`
+--
+
+CREATE TABLE `Message` (
+  `id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` longtext NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Notification`
+--
+
+CREATE TABLE `Notification` (
+  `id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `State`
+--
+
+CREATE TABLE `State` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Topic`
+--
+
+CREATE TABLE `Topic` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `city` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `City`
+--
+ALTER TABLE `City`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `state_fk` (`state_id`);
+
+--
+-- Indexes for table `Event`
+--
+ALTER TABLE `Event`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `topic_fk` (`topic_id`),
+  ADD KEY `event_fk` (`city_id`),
+  ADD KEY `init_fk` (`init_user_id`),
+  ADD KEY `acce_fk` (`acce_user_id`),
+  ADD KEY `eventtype_fk` (`eventtype_id`);
+
+--
+-- Indexes for table `EventType`
+--
+ALTER TABLE `EventType`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Likes`
+--
+ALTER TABLE `Likes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `message_fk` (`message_id`),
+  ADD KEY `liker_user_fk` (`liker_user_id`);
+
+--
+-- Indexes for table `Message`
+--
+ALTER TABLE `Message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_fk` (`user_id`),
+  ADD KEY `event_fk1` (`event_id`);
+
+--
+-- Indexes for table `Notification`
+--
+ALTER TABLE `Notification`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_fk1` (`user_id`),
+  ADD KEY `event_fk2` (`event_id`);
+
+--
+-- Indexes for table `State`
+--
+ALTER TABLE `State`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Topic`
+--
+ALTER TABLE `Topic`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`,`username`),
+  ADD KEY `city_fk` (`city`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `City`
+--
+ALTER TABLE `City`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Likes`
+--
+ALTER TABLE `Likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Message`
+--
+ALTER TABLE `Message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Notification`
+--
+ALTER TABLE `Notification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `State`
+--
+ALTER TABLE `State`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Topic`
+--
+ALTER TABLE `Topic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `City`
+--
+ALTER TABLE `City`
+  ADD CONSTRAINT `state_fk` FOREIGN KEY (`state_id`) REFERENCES `State` (`id`);
+
+--
+-- Constraints for table `Event`
+--
+ALTER TABLE `Event`
+  ADD CONSTRAINT `acce_fk` FOREIGN KEY (`acce_user_id`) REFERENCES `User` (`id`),
+  ADD CONSTRAINT `event_fk` FOREIGN KEY (`city_id`) REFERENCES `City` (`id`),
+  ADD CONSTRAINT `eventtype_fk` FOREIGN KEY (`eventtype_id`) REFERENCES `EventType` (`id`),
+  ADD CONSTRAINT `init_fk` FOREIGN KEY (`init_user_id`) REFERENCES `User` (`id`),
+  ADD CONSTRAINT `topic_fk` FOREIGN KEY (`topic_id`) REFERENCES `Topic` (`id`);
+
+--
+-- Constraints for table `Likes`
+--
+ALTER TABLE `Likes`
+  ADD CONSTRAINT `liker_user_fk` FOREIGN KEY (`liker_user_id`) REFERENCES `User` (`id`),
+  ADD CONSTRAINT `message_fk` FOREIGN KEY (`message_id`) REFERENCES `Message` (`id`);
+
+--
+-- Constraints for table `Message`
+--
+ALTER TABLE `Message`
+  ADD CONSTRAINT `event_fk1` FOREIGN KEY (`event_id`) REFERENCES `Event` (`id`),
+  ADD CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`);
+
+--
+-- Constraints for table `Notification`
+--
+ALTER TABLE `Notification`
+  ADD CONSTRAINT `event_fk2` FOREIGN KEY (`event_id`) REFERENCES `Event` (`id`),
+  ADD CONSTRAINT `user_fk1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`);
+
+--
+-- Constraints for table `User`
+--
+ALTER TABLE `User`
+  ADD CONSTRAINT `city_fk` FOREIGN KEY (`city`) REFERENCES `City` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
