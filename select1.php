449ce24a2e430ef077d1db9f3415e2875c593f9c
nikhil <nikhil.badani@gmail.com>
2018-06-18 13:10:37 +0530

    state and city in sql file added

---
+++ select1.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+<script src="jquery.min.js"></script>
+</head>
+	
+<body>
+
+
+Type :<select name="z" id="z">
+
+<?php 
+ 
+$c=mysqli_connect('localhost','root','','tosscall');
+
+$s="select * from topic";
+
+$ck=mysqli_query($c,$s);
+
+while($r=mysqli_fetch_array($ck))
+{
+	echo"<option value=$r[name]>
+	       $r[name]
+	     </option>";
+ 
+}
+?>
+</select>
+
+
+	City :<select name="y" id="y">
+
+
+<?php 
+ 
+$c=mysqli_connect('localhost','root','','tosscall');
+
+$s1="select * from city ";
+
+$ck1=mysqli_query($c,$s1);
+
+while($r1=mysqli_fetch_array($ck1))
+{
+	echo"<option value=$r1[name]>
+	       $r1[name]
+	     </option>";
+
+}
+?>
+</select>
+
+	<input type="button" name="b1" id="b1" value="Show">
+	
+<p id="x"></p>
+
+
+<script type="text/javascript">
+	
+
+$(document).ready(function(){
+		$("#b1").click(function(){
+			var m=$("#z").val();
+			var n=$("#y").val();
+
+			$.post('select2.php',{k1:m,k2:n},function(data){
+				$("#x").html(data);
+			});
+		});
+	});
+
+
+</script>
+</body>
+</html>
---
+++ select2.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+$a=$_REQUEST["k1"];
+$b=$_REQUEST["k2"];
+
+$c=mysqli_connect('localhost','root','','tosscall');
+
+
+$s2="select * from event where eventtype_id=$a and city_id=$b and status=1";
+
+$ck2=mysqli_query($c,$s2);
+
+echo"<table>";
+echo"<tr>   <td> Name </td><td> Type </td><td> Initiator </td><td> Topic </td><td> Favour </td>
+<td> Date </td><td> Time </td> <td></td>     </tr>   ";
+
+while ($r2=mysqli_fetch_array($ck2)) 
+{
+	echo"<tr> <td> $r2[init_user_id] </td><td> $r2[eventtype_id] </td><td> $r2[topic_id] </td><td>
+	 $r2[favour] </td><td> $r2[date] </td><td> $r2[time] </td> 
+	<td> <input type='button' id='b2' value='Join'> </td> </tr>   ";
+}
+
+
+
+ ?>
