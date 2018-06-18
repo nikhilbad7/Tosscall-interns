6a9ad896e6c69602f7c590abeced99cb50fbcdc3
am-ansh <40288678+am-ansh@users.noreply.github.com>
2018-06-18 14:02:00 +0530

    Add files via upload

---
+++ select1.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['u']))
+{
+echo "<script>window.location='login.php'</script>";
+}
+
+?>
+
+
+<html>
+
+<body>
+<form>
+<h1> <input type="submit" name="logout" value="Logout"> </h1>
+</form>
+<br>
+
+<?php
+
+if(isset($_REQUEST['b1']))
+{
+echo"<script>window.location='logout.php'</script>";
+}
+
+?>
+
+
+
+
+Type :<select name="z" id="z">
+
+<?php 
+ 
+$c=mysqli_connect('localhost','root','','tosscall');
+
+$s="select * from topic";
+
+$ck=mysqli_query($c,$s);
+
+while($r=mysqli_fetch_array($ck))
+{
+	echo"<option value='$r[name]'>
+	       $r[name]
+	     </option>";
+ 
+}
+?>
+</select>
+
+
+
+City :<select name="y" id="y">
+
+
+<?php 
+ 
+$c=mysqli_connect('localhost','root','','tosscall');
+
+$s1="select * from city ";
+
+$ck1=mysqli_query($c,$s1);
+
+while($r1=mysqli_fetch_array($ck1))
+{
+	echo"<option value='$r1[name]'>
+	       $r1[name]
+	     </option>";
+
+}
+?>
+</select>
+
+<input type="button" name="b1" id="b1" value="Show">
+
+<p id="x"></p>
+
+
+<script type="text/javascript">
+	
+
+$(document).ready(function(){
+		$("#b1").click(function(){
+			var m=$("#z").val();
+			var n=$("#y").val();
+
+			$.post('select2.php',{k1:m,k2:n},function(data){
+				$("#x").html(data);
+			});
+		});
+	});
+
+
+</script>
+</body>
+</html>
