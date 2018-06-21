f2f879adfabd38de3530ade18d77a13dc090d932
nikhil <nikhil.badani@gmail.com>
2018-06-21 13:15:38 +0530

    opponent file added

--- ajax_calls/ajax_states.php
+++ ajax_calls/ajax_states.php
@@ -2,22 +2,26 @@
 <?php 
 require_once('../config.inc.php');
 
-$conn   =   mysql_connect("localhost","root","");
+$returnArray = [];
+$conn   =   mysql_connect("localhost","root","rut@localhost");
 if ($conn)
 {
-echo "Conencted Successfully";
-}
-mysql_select_db('tosscall');
-$sql    =   "SELECT name FROM State WHERE status=1;";
+
+mysql_select_db('tosscall_db');
+$sql    =   "SELECT name FROM state WHERE status=1;";
 $result =   mysql_query($sql);
-if (!$result)
+if ($result)
 {
-    die("Could not get data: ".mysql_error());
-    exit();
+    
+while ($row[] =   mysql_fetch_assoc($result))
+{
+    $returnArray = $row; 
+}
+if($returnArray)
+{
+echo json_encode($returnArray);
+}
 
 }
-while ($row =   mysql_fetch_array($result))
-{
- 
 }
 ?>
