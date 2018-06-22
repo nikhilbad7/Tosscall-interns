a3104e4c09f8d70bf9e4525e0560f069fcd664f4
nikhil <nikhil.badani@gmail.com>
2018-06-22 19:40:53 +0530

    mydiscussion link added to every pages

--- start.php
+++ start.php
@@ -12,12 +12,12 @@ echo "<script>window.location='login.php'</script>";
 	</head>
 	<body>
 		<ul>
-			<!--<li><a href="start.php">Start</a></li>-->
+			<li><a href="home.php">Home</a></li>
 			<li><a href="select1.php">Select</a></li>
-			<li><a href="#">Watch</a></li>
+			<li><a href="Watch.php">Watch</a></li>
 			<li><a href="List.php">List</a></li>
 			<li><a href="#">Notification</a></li>
-			<li><a href="#">MyDiscussion</a></li>
+			<li><a href="Discussionlist.php">MyDiscussion</a></li>
 			<li><a href="logout.php">Logout</a></li>
 		</ul>
 		<?php
