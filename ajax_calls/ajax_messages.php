9cd4f5b7ae8eaa96ac4b1355fe4479b3eee91173
Poonam Chand Sahu <poonamchandsahu.ssgi@gmail.com>
2018-06-26 13:25:37 +0530

    added ajax_message.php and ajax_message.receiver.php

---
+++ ajax_calls/ajax_messages.php
@@ -0,0 +1,35 @@
+
+<?php 
+require('../include/config.inc.php');
+if($_SERVER['REQUEST_METHOD']=='POST' && isset($_SESSION['username'])&& isset($_REQUEST['event_id'])&& isset($_REQUEST['date_time']))
+{
+    $user = $_SESSION['username'];
+    $eventId = $_REQUEST['event_id'];
+    $dateTime = $_REQUEST['date_time'];
+$returnArray = [];
+$conn   =   mysqli_connect($db_host,$db_username,$db_password,$db_name);
+
+if ($conn)
+{
+$sql    =   "SELECT id, user, message, date, time FROM message WHERE status=1 and event_id=";
+$sql    .= $eventId." and concat(date,' ',time) < ";
+$sql    .= "'".$dateTime."' and not user =";
+$sql    .= "'".$user."';";
+echo $sql;
+$result =   mysqli_query($conn,$sql);
+if ($result)
+{
+    
+while ($row[] =   mysqli_fetch_assoc($result))
+{
+    $returnArray = $row; 
+}
+if($returnArray)
+{
+echo json_encode($returnArray);
+}
+
+}
+}
+}
+?>
