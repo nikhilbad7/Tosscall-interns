c75b7133c7ca6099d44acabe62181ffbb547fed7
nikhil <nikhil.badani@gmail.com>
2018-07-11 15:11:03 +0530

    ajax_status_update added

--- ajax_calls/ajax_status_update.php
+++ ajax_calls/ajax_status_update.php
@@ -14,18 +14,29 @@ if($rs){
  $currentdt = date('Y-m-d H:i:s');
  $starttime = $r['mergedatetime'];
  $endtime = $r['timeout'];
- if((($starttime <= $currentdt) && ($currentdt <= $endtime)) && $r['status']==2)
+
+ if((($starttime <= $currentdt) && ($currentdt <= $endtime)) && $r['status']==2 && ($r['init_user']==$username || $r['acce_user']==$username))
  {
  		$query0="Update event set status=3 where id=$r[id]";
  		$rs0=mysqli_query($c,$query0);
+
+ 		$query00="insert into live (id,user,status) values ('$r[id]','$r[init_user]',1)";
+    	$rs00 = mysqli_query($c,$query00);
+
+
+ 		$query11="insert into live (id,user,status) values ('$r[id]','$r[acce_user]',0)";
+    	$rs11 = mysqli_query($c,$query11);
+    	header('Location: discussion.php');
  		//if($rs0){echo " changed from 2 to 3";}
  }
- 	elseif (($starttime <= $currentdt) && $r['status']==1) 
+ 	elseif (($starttime <= $currentdt) && $r['status']==1 ) 
  	{
  		$query1="Update event set status=0 where id=$r[id]";
  		$rs1=mysqli_query($c,$query1);
  		//if($rs1){echo " changed from 1 to 0 <br>";}
  	}
+
+
 
   elseif (($currentdt >= $endtime) && $r['status']==2) 
  {
@@ -35,10 +46,20 @@ if($rs){
  	    //if($rs2){echo " changed from 2 to 0";}
  }
 
+ /* elseif (($currentdt >= $endtime) && $r['status']==2 && ($r['init_user']==$username || $r['acce_user']==$username)) 
+ {
+ 	 	$query2="Update event set status=3 where id=$r[id]";
+ 	 	//opponent got but discussion never started
+ 	    $rs2=mysqli_query($c,$query2);
+ 	    //if($rs2){echo " changed from 2 to 0";}
+ }*/
+
  elseif (($currentdt >= $endtime) && $r['status']==3) 
  {
  	 	$query3="Update event set status=4 where id=$r[id]";
  	    $rs3=mysqli_query($c,$query3);
+ 	    $query31="delete from live where id=$r[id]";
+ 	    $rs31=mysqli_query($c,$query31);
  	   //if($rs3){ echo " changed from 3 to 4";}
  }
 }
